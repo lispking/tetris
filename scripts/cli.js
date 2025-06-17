d11a2557a2637d239c7c9901bfbb6be4470da3eb
lispking <lispking@qq.com>
2025-06-17 20:31:25 +0800

    update nft contract

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,6 +1,8 @@
 const { task } = require("hardhat/config");
 const { deployAndUpdateConfig, upgradeAndUpdateConfig } = require("./deployUtils");
 
+const contractAddress = "0x11d7824F9dca07bbBcaC5eBC9Edc6818Da00140d";
+
 task("deploy", "Deploy TetrisNFT")
   .setAction(async (taskArgs, hre) => {
     await deployAndUpdateConfig(hre, "TetrisNFT");
@@ -8,13 +10,12 @@ task("deploy", "Deploy TetrisNFT")
 
 task("upgrade", "Upgrade TetrisNFT")
   .setAction(async (taskArgs, hre) => {
-    await upgradeAndUpdateConfig(hre, "TetrisNFT", "");
+    await upgradeAndUpdateConfig(hre, "TetrisNFT", contractAddress);
   });
 
 task("setBaseURI", "Set Base URI")
   .setAction(async (taskArgs, hre) => {
     const TetrisNFT = await hre.ethers.getContractFactory("TetrisNFT");
-    const contractAddress = "0xB9a76beEA2A161C3C22EFa2247977E17bB2F8D2f";
     const contract = await TetrisNFT.attach(contractAddress);
     await contract.setBaseURI("https://tetris-battle.netlify.app/nfts/");
     console.log("Base URI set successfully");
@@ -23,7 +24,6 @@ task("setBaseURI", "Set Base URI")
 task("checkBaseURI", "Check Base URI")
   .setAction(async (taskArgs, hre) => {
     const TetrisNFT = await hre.ethers.getContractFactory("TetrisNFT");
-    const contractAddress = "0xB9a76beEA2A161C3C22EFa2247977E17bB2F8D2f";
     const contract = await TetrisNFT.attach(contractAddress);
     const baseURI = await contract.baseURI();
     console.log("Contract Base URI:", baseURI);

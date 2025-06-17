b0999b200034877faf9f4c28e2e6d042e434fd8b
lispking <lispking@qq.com>
2025-06-17 20:27:30 +0800

    add setBaseURI and checkBaseURI tools

--- scripts/cli.js
+++ scripts/cli.js
@@ -10,3 +10,21 @@ task("upgrade", "Upgrade TetrisNFT")
   .setAction(async (taskArgs, hre) => {
     await upgradeAndUpdateConfig(hre, "TetrisNFT", "");
   });
+
+task("setBaseURI", "Set Base URI")
+  .setAction(async (taskArgs, hre) => {
+    const TetrisNFT = await hre.ethers.getContractFactory("TetrisNFT");
+    const contractAddress = "0xB9a76beEA2A161C3C22EFa2247977E17bB2F8D2f";
+    const contract = await TetrisNFT.attach(contractAddress);
+    await contract.setBaseURI("https://tetris-battle.netlify.app/nfts/");
+    console.log("Base URI set successfully");
+  })
+
+task("checkBaseURI", "Check Base URI")
+  .setAction(async (taskArgs, hre) => {
+    const TetrisNFT = await hre.ethers.getContractFactory("TetrisNFT");
+    const contractAddress = "0xB9a76beEA2A161C3C22EFa2247977E17bB2F8D2f";
+    const contract = await TetrisNFT.attach(contractAddress);
+    const baseURI = await contract.baseURI();
+    console.log("Contract Base URI:", baseURI);
+  });
